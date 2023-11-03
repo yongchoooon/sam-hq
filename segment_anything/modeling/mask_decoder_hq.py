293a6ce81e23510231777489774f9c7f6cf86464
yongchoooon <sod7050@gmail.com>
2023-11-03 15:01:56 +0900

    Mask decoder hq comments

--- segment_anything/modeling/mask_decoder_hq.py
+++ segment_anything/modeling/mask_decoder_hq.py
@@ -43,13 +43,13 @@ class MaskDecoderHQ(nn.Module):
             used to predict mask quality
         """
         super().__init__()
-        self.transformer_dim = transformer_dim
+        self.transformer_dim = transformer_dim ## 256
         self.transformer = transformer
 
-        self.num_multimask_outputs = num_multimask_outputs
+        self.num_multimask_outputs = num_multimask_outputs ## 3
 
         self.iou_token = nn.Embedding(1, transformer_dim)
-        self.num_mask_tokens = num_multimask_outputs + 1
+        self.num_mask_tokens = num_multimask_outputs + 1 ## 4
         self.mask_tokens = nn.Embedding(self.num_mask_tokens, transformer_dim)
 
         self.output_upscaling = nn.Sequential(
@@ -71,23 +71,28 @@ class MaskDecoderHQ(nn.Module):
         )
 
         # HQ-SAM parameters
-        self.hf_token = nn.Embedding(1, transformer_dim) # HQ-Ouptput-Token
+        self.hf_token = nn.Embedding(1, transformer_dim)# HQ-Ouptput-Token
+        ## 1. token 정의
         self.hf_mlp = MLP(transformer_dim, transformer_dim, transformer_dim // 8, 3) # corresponding new MLP layer for HQ-Ouptput-Token
+        ## 2. mask prediction 직전의 MLP 정의
         self.num_mask_tokens = self.num_mask_tokens + 1
         
         # three conv fusion layers for obtaining HQ-Feature
-        self.compress_vit_feat = nn.Sequential(
+        ## 3. HQ-Feature를 얻기 위한 3개의 conv fusion layer 정의
+        ## 3-1. upscaling : vit early feature
+        self.compress_vit_feat = nn.Sequential( 
                                         nn.ConvTranspose2d(vit_dim, transformer_dim, kernel_size=2, stride=2),
                                         LayerNorm2d(transformer_dim),
                                         nn.GELU(), 
                                         nn.ConvTranspose2d(transformer_dim, transformer_dim // 8, kernel_size=2, stride=2))
-        
+        ## 3-2. upscaling : vit final feature
         self.embedding_encoder = nn.Sequential(
                                         nn.ConvTranspose2d(transformer_dim, transformer_dim // 4, kernel_size=2, stride=2),
                                         LayerNorm2d(transformer_dim // 4),
                                         nn.GELU(),
                                         nn.ConvTranspose2d(transformer_dim // 4, transformer_dim // 8, kernel_size=2, stride=2),
                                     )
+        ## 3-3. mask feature
         self.embedding_maskfeature = nn.Sequential(
                                         nn.Conv2d(transformer_dim // 8, transformer_dim // 4, 3, 1, 1), 
                                         LayerNorm2d(transformer_dim // 4),
@@ -122,9 +127,11 @@ class MaskDecoderHQ(nn.Module):
           torch.Tensor: batched predictions of mask quality
         """
         vit_features = interm_embeddings[0].permute(0, 3, 1, 2) # early-layer ViT feature, after 1st global attention block in ViT
+        ## 4. interm_embeddings[0]이 early-layer ViT feature
         hq_features = self.embedding_encoder(image_embeddings) + self.compress_vit_feat(vit_features)
+        ## 5. final layer feature map + early layer feature map (= global local fusion) / But 아직 mask feature는 합쳐지지 않았음
 
-        masks, iou_pred = self.predict_masks(
+        masks, iou_pred = self.predict_masks( ## 6. Go
             image_embeddings=image_embeddings,
             image_pe=image_pe,
             sparse_prompt_embeddings=sparse_prompt_embeddings,
@@ -147,11 +154,13 @@ class MaskDecoderHQ(nn.Module):
             iou_pred = iou_pred[:,mask_slice]
             masks_sam = masks[:,mask_slice]
 
+        ## 12. return output HQ-mask
         masks_hq = masks[:,slice(self.num_mask_tokens-1, self.num_mask_tokens)]
         if hq_token_only:
             masks = masks_hq
         else:
             masks = masks_sam + masks_hq
+        ## 13. Error correction : 두 mask를 합함
         # Prepare output
         return masks, iou_pred
 
@@ -168,6 +177,7 @@ class MaskDecoderHQ(nn.Module):
         output_tokens = torch.cat([self.iou_token.weight, self.mask_tokens.weight, self.hf_token.weight], dim=0)
         output_tokens = output_tokens.unsqueeze(0).expand(sparse_prompt_embeddings.size(0), -1, -1)
         tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)
+        ## 7. token 4가지를 고려 (prompt token, output token, HQ-Output token, IoU token)
 
         # Expand per-image data in batch direction to be per-mask
         src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
@@ -176,6 +186,7 @@ class MaskDecoderHQ(nn.Module):
         b, c, h, w = src.shape
 
         # Run the transformer
+        ## 8. prompt embedding, positional encoding이 합쳐진 image embedding과 4가지 token을 2-layer Transformer-based Decoder에 넣기
         hs, src = self.transformer(src, pos_src, tokens)
         iou_token_out = hs[:, 0, :]
         mask_tokens_out = hs[:, 1 : (1 + self.num_mask_tokens), :]
@@ -183,14 +194,14 @@ class MaskDecoderHQ(nn.Module):
         # Upscale mask embeddings and predict masks using the mask tokens
         src = src.transpose(1, 2).view(b, c, h, w)
 
-        upscaled_embedding_sam = self.output_upscaling(src)
-        upscaled_embedding_hq = self.embedding_maskfeature(upscaled_embedding_sam) + hq_features.repeat(b,1,1,1)
+        upscaled_embedding_sam = self.output_upscaling(src) ## 9. mask feature를 1024x1024로 upsampling
+        upscaled_embedding_hq = self.embedding_maskfeature(upscaled_embedding_sam) + hq_features.repeat(b,1,1,1) ## 9. 추가됨 : 이게 HQ_Features
 
         hyper_in_list: List[torch.Tensor] = []
         for i in range(self.num_mask_tokens):
             if i < self.num_mask_tokens - 1:
                 hyper_in_list.append(self.output_hypernetworks_mlps[i](mask_tokens_out[:, i, :]))
-            else:
+            else: ## 10. HQ-Features는 HQ MLP를 통과한 후 hyper_in_list에 추가
                 hyper_in_list.append(self.hf_mlp(mask_tokens_out[:, i, :]))
 
         hyper_in = torch.stack(hyper_in_list, dim=1)
@@ -198,6 +209,7 @@ class MaskDecoderHQ(nn.Module):
 
         masks_sam = (hyper_in[:,:self.num_mask_tokens-1] @ upscaled_embedding_sam.view(b, c, h * w)).view(b, -1, h, w)
         masks_sam_hq = (hyper_in[:,self.num_mask_tokens-1:] @ upscaled_embedding_hq.view(b, c, h * w)).view(b, -1, h, w)
+        ## 11. HQ Features와 MLP를 통과한 mask feature를 dot product => HQ mask prediction
         masks = torch.cat([masks_sam,masks_sam_hq],dim=1)
         # Generate mask quality predictions
         iou_pred = self.iou_prediction_head(iou_token_out)
